b1b0244c38cdd648f2f9995785702f581563ca7f
Claus Hansen <claus@multimedion.dk>
2017-11-30 18:42:18 +0100

    implementet cache system

--- install.mysql.utf8.sql
+++ install.mysql.utf8.sql
@@ -5,3 +5,10 @@ CREATE TABLE IF NOT EXISTS `#__abuseipdb_entries` (
   `category` varchar(255) DEFAULT NULL,
   KEY `idx_category_date_priority` (`category`,`date`,`priority`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `#__abuseipdb_cache` (
+  `IP` varchar(255) NOT NULL,
+  `blocked` int(11) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`IP`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
